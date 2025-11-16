8260a26cc5e23e30a2e1ab1b00b9a8c9b981f4c6
Ilyaexsite <ilyatsarygin64@gmail.com>
2025-11-16 18:30:02 +0300

    end

--- src/view.js
+++ src/view.js
@@ -200,10 +200,12 @@ const updatePostsList = (posts, readPosts, onPreviewClick) => {
     
     return `
     <div class="list-group-item d-flex justify-content-between align-items-start border-0">
-      <a href="${post.link}" class="${titleClass} text-decoration-none" target="_blank" rel="noopener noreferrer" data-testid="post-link">
-        ${post.title}
-      </a>
-      <button type="button" class="btn btn-outline-primary btn-sm ms-2" data-post-id="${post.id}" data-testid="view-button">
+      <div class="me-auto text-decoration-none">
+        <a href="${post.link}" class="${titleClass}" target="_blank" rel="noopener noreferrer" data-testid="post-link">
+          ${post.title}
+        </a>
+      </div>
+      <button type="button" class="btn btn-outline-primary btn-sm" data-post-id="${post.id}" data-testid="view-button">
         Просмотр
       </button>
     </div>
